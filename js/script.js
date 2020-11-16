c51ebaaf2f4c1fa6436867166d456490e9160751
Sergey Kh <falko.serega2016@yandex.ru>
2020-11-16 12:39:52 +0300

    native js update

---
+++ js/script.js
@@ -0,0 +1,34 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
+import calc   from './modules/calc';
+import cards  from './modules/cards';
+import forms  from './modules/forms';
+import modal  from './modules/modal';
+import slider from './modules/slider';
+import tabs   from './modules/tabs';
+import timer  from './modules/timer';
+import {openModal} from './modules/modal';
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  
+  const modalTimerId = setTimeout( () => openModal('.modal', modalTimerId), 500000 ); 
+
+  calc();
+  cards();
+  forms('form', modalTimerId);
+  modal('[data-modal]', '.modal', modalTimerId); 
+  tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+  timer('.timer', '2020-12-31');
+  slider({
+    container: '.offer__slider',
+    nextArrow: '.offer__slider-next',
+    prevArrow: '.offer__slider-prev',
+    slide: '.offer__slide',
+    totalCounter: '#total',
+    currentCounter: '#current',
+    wrapper: '.offer__slider-wrapper',
+    field: '.offer__slider-inner'
+  });
+});
